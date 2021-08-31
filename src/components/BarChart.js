1b1c1ad83c81b71dcb21a7d25ed586ed413cb5d0
fahimahammed <fahimahammed.cse@gmail.com>
2021-08-31 14:44:32 +0600

    adde

---
+++ src/components/BarChart.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+
+const BarChart = () => {
+
+    const data ={ 
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
+        datasets: [
+            {
+                label: 'Sales of 2020',
+                data : [12, 9, 5, 11, 4],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                ],
+                borderWidth: 1,
+            },
+            {
+                label: 'Sales of 2021',
+                data : [15, 11, 3, 8, 7],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                ],
+                borderWidth: 1,
+            }
+        ]
+    };
+
+    const options = {
+        scales: {
+            yAxes: [
+                {
+                    ticks: {
+                        biginAtZero: true
+                    }
+                }
+            ]
+        }
+    };
+
+    return (
+        <div>
+            <h1>Bar Chart</h1>
+            <Bar data = { data }  options = {options}/>
+        </div>
+    );
+};
+
+export default BarChart;
---
+++ src/components/LineChart.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+
+const LineChart = () => {
+    const data = {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
+        datasets: [
+            {
+                label: 'Seles for 2020 (M)',
+                data: [ 3, 2, 2, 1, 5],
+                fill: true,
+                borderColor: ['rgba(255, 206, 86, 0.2)'],
+                backgroundColor: ['rgba(255, 206, 86, 0.2)'],
+                pointBackgroundColor: ['rgba(255, 206, 86, 0.2)'],
+                pointBorderColor: ['rgba(255, 206, 86, 0.2)']
+            },
+            {
+                label: 'Sales for 2019 (M)',
+                data: [1, 3, 3, 2, 1 ],
+                fill: true,
+                borderColor: ['rgba(24, 206, 83, 0.2)'],
+                backgroundColor: ['rgba(24, 206, 83, 0.2)'],
+                pointBackgroundColor: ['rgba(24, 206, 83, 0.2)'],
+                pointBorderColor: ['rgba(24, 206, 83, 0.2)']
+            },
+        ],
+    };
+
+    const options = {
+        title: {
+            display: true,
+            text: 'Line Chart'
+        },
+        scales: {
+            yAxes: [
+                {
+                    ticks: {
+                        // min: 0,
+                        // max: 6,
+                        // stepSize: 1
+                        beginAtZero: true,
+                    },
+                },
+            ],
+        },
+    };
+    return (
+        
+            <div>
+                <h1>Line Chart</h1>
+                <Line data={data} options = {options} />
+            </div>
+        
+    );
+};
+
+export default LineChart;
